4991e190ae8dcba3cd05a057d9ef1ba38220a160
ramibelhadjsalem <60022426+ramibelhadjsalem@users.noreply.github.com>
2022-07-13 14:01:07 +0200

    login finiched

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
-
-  constructor() { }
-
+  loginForm !: FormGroup ; 
+  constructor(private fb :FormBuilder) { }
+ 
   ngOnInit(): void {
+    this.initLogin()
+  }
+
+  initLogin(){
+    this.loginForm = this.fb.group({
+      username:['',Validators.required],
+      password : ['',Validators.required]
+    })
   }
 
 }

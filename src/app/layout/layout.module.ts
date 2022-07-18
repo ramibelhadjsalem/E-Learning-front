75b751717e88b9bf053e9cc65cb77fac30445cb0
ramibelhadjsalem <60022426+ramibelhadjsalem@users.noreply.github.com>
2022-07-18 21:50:41 +0200

    some midification

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,12 +1,12 @@
-import { ResetPasswordComponent } from './../shared/reset-password/reset-password.component';
-import { SharedModule } from './../shared/shared.module';
+
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 import { RouterModule } from '@angular/router';
 import { MainlayoutComponent } from './mainlayout/mainlayout.component';
 import { ErrorLayoutComponent } from './error-layout/error-layout.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
